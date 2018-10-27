ff9f587734e8433a9995eae3b5655b831690f4ed
Rexman17 <a.stone1794@gmail.com>
2018-10-27 17:17:36 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = [] # new class array
 
   def call(env)
     resp = Rack::Response.new
@@ -10,6 +11,25 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
+    elsif req.path.match(/cart/) # CART
+      if @@cart.empty? # if cart is empty..
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item| # if it's not empty, iterate and print each item
+          resp.write "#{item}\n"
+        end
+      end
+
+    elsif req.path.match(/add/) # ADD
+      item = req.params["item"]
+      if @@items.include?(item)
+        @@cart << item
+        resp.write "added #{item}"
+      else
+        resp.write "We don't have that item"
+      end
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
@@ -25,6 +45,6 @@ class Application
       return "#{search_term} is one of our items"
     else
       return "Couldn't find #{search_term}"
-    end
+    end # end of first if statement
   end
-end
+end # end of Application class
